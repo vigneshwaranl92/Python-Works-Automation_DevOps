f89b14eb9fe559d8d60fd347316bcc5684c02ae7
vigneshwaranl92 <vickyragu@gmail.com>
2018-08-19 12:03:26 +0200

    Update Random_Generators.py

--- Random_Generators.py
+++ Random_Generators.py
@@ -1,5 +1,8 @@
 from random import*
+#from random import*   <-- You can do this in order to avoid typing module explicitly
 import keyword
+
+
 
 for i in range (10):
     print(randint(0,6), randint(0,20),randint(0,30),randint(1,6),randint(3,6), sep="")
@@ -7,5 +10,6 @@ for i in range (10):
 print(i)
 
 
-
+#Keyword.kwlist is used to display all the keywords which is in build in python
+#According to python version3, there are 33 keywords
 keyword.kwlist
